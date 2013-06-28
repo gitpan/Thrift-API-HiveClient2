0db7a6470a45c9be0f44989af63fca994fd13943
David Morel <david.morel@amakuru.net>
2013-06-28 13:18:28 +0000

    Import of DMOR/Thrift-API-HiveClient2-0.005 from CPAN.
    gitpan-cpan-distribution: Thrift-API-HiveClient2
    gitpan-cpan-version:      0.005
    gitpan-cpan-path:         DMOR/Thrift-API-HiveClient2-0.005.tar.gz
    gitpan-cpan-author:       DMOR
    gitpan-cpan-maturity:     released

--- lib/Thrift/API/HiveClient2/Constants.pm
+++ lib/Thrift/API/HiveClient2/Constants.pm
@@ -5,7 +5,7 @@
 #
 package Thrift::API::HiveClient2::Constants;
 {
-  $Thrift::API::HiveClient2::Constants::VERSION = '0.004';
+  $Thrift::API::HiveClient2::Constants::VERSION = '0.005';
 }
 {
   $Thrift::API::HiveClient2::Constants::DIST = 'Thrift-API-HiveClient2';
@@ -71,7 +71,7 @@ Thrift::API::HiveClient2::Constants
 
 =head1 VERSION
 
-version 0.004
+version 0.005
 
 =head1 AUTHOR
 
